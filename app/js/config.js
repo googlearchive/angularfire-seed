fabf1680b725641de48747de00259ef0fad296ee
katowulf <katowulf@gmail.com>
2013-08-06 15:16:46 -0700

    push config to demo

--- app/js/config.js
+++ app/js/config.js
@@ -4,4 +4,5 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp.config', [])
    .constant('version', '0.1')
-   .constant('FBURL', 'https://INSTANCE.firebaseio.com');
+   //.constant('FBURL', 'https://INSTANCE.firebaseio.com');
+   .constant('FBURL', 'https://angularfire-seed.firebaseio.com/');
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -22,24 +22,44 @@ describe('my app', function() {
 
 
     it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
+      expect(element('[ng-view] h2:first').text()).
+        toMatch(/View 1/);
     });
 
   });
 
 
   describe('view2', function() {
-
     beforeEach(function() {
       browser().navigateTo('#/view2');
     });
 
 
     it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
+      expect(element('[ng-view] h2:first').text()).
+        toMatch(/View 2/);
     });
 
   });
+
+   describe('login', function() {
+      beforeEach(function($provide) {
+         console.log('$provide', $provide); //debug
+         browser().navigateTo('#/login');
+      });
+
+      it('should render login when user navigates to /login', function() {
+         expect(element('[ng-view] h2:first').text()).toMatch('Login Page');
+      });
+
+      it('should reveal the confirm password box when "create account" is clicked', function() {
+
+      });
+
+      it('should redirect to /account after login success', function() {
+
+         //element('[ng-view] input[ng-model="email"]').val('test@test.com');
+
+      });
+   });
 });
