1640e08fd8abf19b8f1d279ce9e79bd03f2ed60a
katowulf <katowulf@gmail.com>
2013-12-10 17:01:57 -0700

    update Karma conf to latest, fixes #1

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,20 +1,37 @@
-basePath = '../';
+module.exports = function(config){
+   config.set({
+      basePath : '../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
-];
+      files : [
+         'app/lib/angular/angular.js',
+         'app/lib/angular/angular-*.js',
+         'test/lib/angular/angular-mocks.js',
+         'app/js/**/*.js',
+         'test/unit/**/*.js'
+      ],
 
-autoWatch = true;
+      exclude : [
+         'app/lib/angular/angular-loader.js',
+         'app/lib/angular/*.min.js',
+         'app/lib/angular/angular-scenario.js'
+      ],
 
-browsers = ['Chrome'];
+      autoWatch : true,
 
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
-};
+      frameworks: ['jasmine'],
+
+      browsers : ['Chrome'],
+
+      plugins : [
+         'karma-junit-reporter',
+         'karma-chrome-launcher',
+         'karma-firefox-launcher',
+         'karma-jasmine'
+      ],
+
+      junitReporter : {
+         outputFile: 'test_out/unit.xml',
+         suite: 'unit'
+      }
+
+   })}
