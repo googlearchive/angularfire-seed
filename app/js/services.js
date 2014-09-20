268db5ed4558028c630c3b03bcd9b5cd4a578686
katowulf <katowulf@gmail.com>
2014-09-20 16:27:33 -0700

    Switch service with factory (it's not a Class called with new)

--- app/js/services.js
+++ app/js/services.js
@@ -8,7 +8,7 @@
       // put your services here!
       // .service('serviceName', ['dependency', function(dependency) {}]);
 
-     .service('messageList', ['fbutil', function(fbutil) {
+     .factory('messageList', ['fbutil', function(fbutil) {
        return fbutil.syncArray('messages', {limit: 10, endAt: null});
      }]);
 
