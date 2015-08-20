7488cbd5d8de7cf255afd1e00f97bf4b3eb9ae82
David Nichols <davenich@gmail.com>
2015-08-20 16:18:07 -0400

    Default redirect to /home

--- app/app.js
+++ app/app.js
@@ -9,7 +9,13 @@ angular.module('myApp', [
     'myApp.chat',
     'myApp.login'
   ])
-
+  
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({
+      redirectTo: '/home'
+    });
+  }])
+  
   .run(['$rootScope', 'Auth', function($rootScope, Auth) {
     // track status of authentication
     Auth.$onAuth(function(user) {
